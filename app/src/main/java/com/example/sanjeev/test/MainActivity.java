c3d3bf00595eaa067c0cc24c43da5cf928d49bd3
sanjeev <sanjeevmsm@gmail.com>
2016-06-21 11:17:40 +0530

    second commit

---
+++ app/src/main/java/com/example/sanjeev/test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sanjeev.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //second commit
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
